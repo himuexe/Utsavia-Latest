75fadbaa7b5798750b5671b834448e401d6bc10b
Himanshu Sharma <sharmaslov@gmail.com>
2025-01-11 23:57:52 +0530

    Small Fix

--- backend/src/controllers/MyUserController.js
+++ backend/src/controllers/MyUserController.js
@@ -9,37 +9,72 @@ const createCurrentUser = async (req, res) => {
   }
 
   try {
+
     const existingUser = await User.findOne({
-      'authMethods': {
-        $elemMatch: {
-          provider: 'local',
-          email: req.body.email
-        }
-      }
+      'authMethods.email': req.body.email
     });
 
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists with this email" });
+      const hasLocalAuth = existingUser.authMethods.some(
+        method => method.provider === 'local'
+      );
+
+      if (hasLocalAuth) {
+        return res.status(400).json({ message: "User already exists with this email" });
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 8);
+      
+      existingUser.authMethods.push({
+        provider: 'local',
+        email: req.body.email,
+        password: hashedPassword
+      });
+
+      // Update user details if they weren't set before
+      if (!existingUser.firstName) existingUser.firstName = req.body.firstName;
+      if (!existingUser.lastName) existingUser.lastName = req.body.lastName;
+
+      await existingUser.save();
+
+      const token = jwt.sign(
+        { 
+          userId: existingUser.id,
+          provider: 'local'
+        }, 
+        process.env.JWT_SECRET_KEY, 
+        {
+          expiresIn: "1d"
+        }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000,
+      });
+
+      return res.status(200).send({ message: "Local authentication added successfully" });
     }
 
     const hashedPassword = await bcrypt.hash(req.body.password, 8);
 
-    const user = new User({
+    const newUser = new User({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       primaryEmail: req.body.email,
       authMethods: [{
         provider: 'local',
         email: req.body.email,
-        password: hashedPassword  
+        password: hashedPassword
       }]
     });
 
-    await user.save();
+    await newUser.save();
 
     const token = jwt.sign(
       { 
-        userId: user.id,
+        userId: newUser.id,
         provider: 'local'
       }, 
       process.env.JWT_SECRET_KEY, 
@@ -57,6 +92,7 @@ const createCurrentUser = async (req, res) => {
     return res.status(200).send({ message: "User registered successfully" });
 
   } catch (error) {
+    console.log(error);
     res.status(500).send({ message: "Something went wrong" });
   }
 };
