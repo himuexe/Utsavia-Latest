06a933924dbf58d7a2037162b092e7302bf837b3
Himanshu Sharma <sharmaslov@gmail.com>
2025-01-12 18:06:31 +0530

    Test Deployement 3 --logout

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -98,6 +98,7 @@ const logout = (req, res) => {
   res.cookie("auth_token", "", {
     expires: new Date(0),
     httpOnly: true,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     secure: process.env.NODE_ENV === "production",
   });
   return res.status(200).json({ message: "Logged out successfully" });
