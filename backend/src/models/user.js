a2371258f3c8fa5fe49ebc7a98d2732ba08d65a8
Himanshu <sharmaslov@gmail.com>
2025-03-23 15:45:55 +0530

    Index bug

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -45,7 +45,8 @@ const userSchema = new mongoose.Schema({
   },
   primaryEmail: { 
     type: String, 
-    required: true 
+    required: true ,
+    unique: true
   },
   addresses: [addressSchema], // Array of addresses
   phone: { 
